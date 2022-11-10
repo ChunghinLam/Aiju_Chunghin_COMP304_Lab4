fc880e6a130734f5ef2a54f338da3d37071cc24b
Chunghin Lam <clam141@my.centennialcollege.ca>
2022-11-10 10:57:48 -0500

    updated ticket activity to save db

---
+++ app/src/main/java/com/example/aiju_chunghin_comp304_lab4/ViewModels/TicketViewModel.java
@@ -0,0 +1,40 @@
+package com.example.aiju_chunghin_comp304_lab4.ViewModels;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.aiju_chunghin_comp304_lab4.Models.Ticket;
+import com.example.aiju_chunghin_comp304_lab4.Repos.TicketRepository;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class TicketViewModel extends AndroidViewModel {
+    private TicketRepository ticketRepository;
+    private LiveData<Integer> insertResult;
+    private LiveData<List<Ticket>> allTickets;
+
+    public TicketViewModel(@NotNull Application application) {
+        super(application);
+
+        ticketRepository = new TicketRepository(application);
+        insertResult = ticketRepository.getInsertResult();
+        allTickets = ticketRepository.getAllTickets();
+    }
+
+    public void insert(Ticket ticket) {
+        ticketRepository.insert(ticket);
+    }
+
+    public LiveData<Integer> getInsertResult() {
+        return insertResult;
+    }
+
+    public LiveData<List<Ticket>> getAllTickets() {
+        return allTickets;
+    }
+
+}
